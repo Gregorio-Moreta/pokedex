f15403f6ac9e38ac8b74096af45882db699c3552
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-10-05 20:50:38 -0400

    Created and updated the index and show routes so that I can see all of the pokemon and their skills on index, and specify one pokemon at a time using the show page which follows the same order as the index page.

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 ///////////////////////////////
 //  Import our dependencies
 ///////////////////////////////
+// Load ENV Variables
+require("dotenv").config() 
 //  We import the express library 
 const express = require('express');
 //  Creating the application object
 const app = express();
 //  Import my pokemon data
-const pokemon = require ("./models/pokemon")
+const pokemon = require("./models/pokemon")
 //  Import method-override
-const methodOverride = reqiure ("method-override")
+//const methodOverride = reqiure("method-override")
 
 ///////////////////////////////
 //  Middleware
@@ -20,19 +22,24 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.static("public"))
 
 //  After the app has been defined use methodOverride. We'll be adding a query parameter to our delete form named _method
-app.use(methodOverride("_method"));
+//app.use(methodOverride("_method"));
 
 ///////////////////////////////
 //  Routes
 ///////////////////////////////
 //  INDEX ROUTE - GET ALL POKEMON
-app.get('/models/pokemon', (req, res) => {
+app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {allPokemon: pokemon, title: "Pokemon - Index Page"})
 })
 
+//  SHOW ROUTE - GET A SPECIFIC POKEMON
+app.get('/pokemon/:indexOfPokemonArray', (req, res) => {
+    res.render("show.ejs", {allPokemon: pokemon[req.params.indexOfPokemonArray]
+    , title: "First - Index Page"})
+})
 
-
-
-app.listen(5000, () => {
+//  Listener
+const PORT = process.env.PORT
+app.listen(PORT, () => {
     console.log('listening')
 })
